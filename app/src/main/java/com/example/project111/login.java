e8ac257633375f96d5bbdda96230e8f1a814867c
irsyad <136949368+Franklnir@users.noreply.github.com>
2024-11-07 06:56:52 +0700

    Update login.java

--- app/src/main/java/com/example/project111/login.java
+++ app/src/main/java/com/example/project111/login.java
@@ -13,7 +13,7 @@ public class login extends AppCompatActivity {
     private EditText etUsername, etPassword;
     private Button btnLogin, btnRegister;
 
-    // Akun yang akan digunakan untuk login
+
     private final String correctUsername = "irsyad26";
     private final String correctPassword = "312310512";
 
@@ -22,23 +22,23 @@ public class login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // Inisialisasi komponen dari XML
+
         etUsername = findViewById(R.id.etusername);
         etPassword = findViewById(R.id.etpassword);
         btnLogin = findViewById(R.id.btnlogin);
         btnRegister = findViewById(R.id.btnregister);
 
-        // Logika saat tombol Login diklik
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String username = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
 
-                // Validasi username dan password
+
                 if (username.equals(correctUsername) && password.equals(correctPassword)) {
                     Toast.makeText(login.this, "Login Berhasil", Toast.LENGTH_SHORT).show();
-                    // Lanjutkan ke MainActivity
+
                     Intent intent = new Intent(login.this, MainActivity.class);
                     startActivity(intent);
                     finish();
@@ -48,11 +48,11 @@ public class login extends AppCompatActivity {
             }
         });
 
-        // Logika saat tombol Register diklik
+
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Pindah ke halaman register
+
                 Intent intent = new Intent(login.this, register.class);
                 startActivity(intent);
             }
